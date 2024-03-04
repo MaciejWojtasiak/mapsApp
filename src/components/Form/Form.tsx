fe7f40fdd0177bfa320ed1aca0ea5912437ab59c
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2024-03-04 11:42:46 +0100

    receiving data from the map

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,53 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Form.module.css';
 import BackButton from '../BackButton/BackButton';
+import useUrlPosition from '../../hooks/useUrlPosition';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loader from '../Loader/Loader';
 
 function Form() {
     const navigate = useNavigate();
+    const [lat,lng] = useUrlPosition();
+    const [cityName,setCityName] = useState('');
+    const [country,setCountry] = useState('');
+    const [date,setDate] = useState(new Date());
+    const [notes,setNotes] = useState('');    
+    const [isFetching, setIsFetching] = useState(false);
+    const [fetchErr, setFetchErr] = useState(false);
+
+    const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+
+    useEffect(()=>{
+       const getData = async () => {
+            try {
+                setIsFetching(true);
+                setFetchErr(false);
+                const response = await axios.get(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+                if(!response.data.countryCode) {
+                    throw new Error(
+                        "That doesn't look like a city. Click somewhere else."
+                    )
+                }
+                setCityName(response.data.city || response.data.locality || '');
+                setCountry(response.data.countryName);
+            }catch (err){
+                setFetchErr(err.message);
+            } finally{
+                setIsFetching(false);
+            }            
+        }
+        getData();        
+    },[lat,lng])
+
+    {if(isFetching) return <Loader />}
+    {if(fetchErr) return<div>{fetchErr}</div>}
 
   return (
     <form action="#" className={styles.form}>
         <div className="form-group">
             <label htmlFor="city">City name</label>
-            <input type="text" name="city" id="city"/>
+            <input type="text" name="city" id="city" value={cityName} onChange={e=>setCityName(e.target.value)}/>
         </div>
 
         <div className="form-group">
@@ -19,7 +57,7 @@ function Form() {
 
         <div className="form-group">
             <label htmlFor="notes">Notes</label>
-            <textarea name="notes" id="notes"/>
+            <textarea name="notes" id="notes" value={notes} onChange={e=>setNotes(e.target.value)}/>
         </div>
         <div className={styles.buttons}>
             <button className='cta'>Add</button>
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,18 +1,19 @@
-import { useSearchParams } from "react-router-dom";
+
+import { useNavigate } from 'react-router-dom';
 import styles from './Map.module.css';
-import { MapContainer, Marker, Popup, useMap} from "react-leaflet";
+import { MapContainer, Marker, Popup, useMap, useMapEvents} from "react-leaflet";
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { useEffect, useState } from "react";
 import {useCities} from '../../context/CitiesContext';
 import useGeolocation from '../../hooks/useGeolocation';
+import useUrlPosition from '../../hooks/useUrlPosition';
+
 
 function Map() {
   const { cities } = useCities();
-  const [mapPosition, setMapPosition] = useState([50,10]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const [mapPosition, setMapPosition] = useState([50,10]); 
   const { isLoading:isLoadingPosition, position:geolocationPosition, getPosition } = useGeolocation();
+  const [lat, lng] = useUrlPosition();
 
   useEffect(()=>{
       if(lat && lng) setMapPosition([lat,lng]);
@@ -28,7 +29,7 @@ function Map() {
   return (
     <div className={styles.mapContainer}>
       {!geolocationPosition && <button onClick={getPosition} className={styles.button}>{isLoadingPosition ? 'Loading...':'Use your position'   }</button>}
-      <MapContainer center={mapPosition} zoom={15} scrollWheelZoom={true} className={styles.map}>
+      <MapContainer center={mapPosition} zoom={20} scrollWheelZoom={true} className={styles.map}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -39,7 +40,8 @@ function Map() {
           <Popup>{city.cityName}</Popup>
         </Marker>
       })}
-      <ChangeCenter position={mapPosition}/>   
+      <ChangeCenter position={mapPosition}/>  
+      <DetectClick />
     </MapContainer>
     </div>
   )
@@ -48,6 +50,14 @@ function Map() {
 function ChangeCenter({position}) {
   const map = useMap();
   map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
   return null;
 }
 
